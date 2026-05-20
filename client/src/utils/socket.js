4a1cf551708baadccc0b9fe5796ef14a9bd076ce
hellenicdev <ikonpanos@gmail.com>
2026-05-20 20:56:11 +0300

    Update production URL to ghost-chat-7i32.onrender.com

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const DEV_URL = 'http://localhost:3001';
-const PROD_URL = 'https://ghost-chat.onrender.com';
+const PROD_URL = 'https://ghost-chat-7i32.onrender.com';
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || (import.meta.env.DEV ? DEV_URL : PROD_URL);
 
 const socket = io(SOCKET_URL, {
